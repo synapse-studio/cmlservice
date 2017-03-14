f78a6220fe2cac3afedbc813c906a12c90f83e00
www-data <politsin@gmail.com>
2017-03-14 23:25:43 +0300

    custom xml query

---
+++ src/Feeds/Parser/Form/CustomParcerForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\cmlservice\Feeds\Parser\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\feeds\Plugin\Type\ExternalPluginFormBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * The configuration form for the CSV parser.
+ */
+class CustomParcerForm extends ExternalPluginFormBase implements ContainerInjectionInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('stream_wrapper_manager'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['query'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Query'),
+      '#default_value' => $this->plugin->getConfiguration('query'),
+      '#description' => 'Example: "Классификатор/Бренды/Бренд" or "Классификатор/Категории"',
+    ];
+    return $form;
+  }
+
+}
