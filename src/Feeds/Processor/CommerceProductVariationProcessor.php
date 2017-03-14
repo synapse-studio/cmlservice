4e22d0084e88e87b1ed28942b9ca28c3be9860b1
Anatoly <politsin@gmail.com>
2017-03-14 17:26:25 +0300

    catalog parents

--- src/Feeds/Processor/CommerceProductVariationProcessor.php
+++ src/Feeds/Processor/CommerceProductVariationProcessor.php
@@ -14,7 +14,7 @@ use Drupal\feeds\Feeds\Processor\EntityProcessorBase;
  *   title = @Translation("Product variation"),
  *   description = @Translation("Product variations."),
  *   entity_type = "commerce_product_variation",
- *   arguments = {"@entity.manager", "@entity.query"},
+ *   arguments = {"@entity.manager", "@entity.query", "@entity_type.bundle.info"},
  *   form = {
  *     "configuration" = "Drupal\feeds\Feeds\Processor\Form\DefaultEntityProcessorForm",
  *     "option" = "Drupal\feeds\Feeds\Processor\Form\EntityProcessorOptionForm",
